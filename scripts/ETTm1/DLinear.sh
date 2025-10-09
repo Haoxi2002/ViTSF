7ab4011d6a4789408d9f06ccf1d3ba72c0c15d37
Haoxi2002 <3020001195@tju.edu.cn>
2025-10-09 17:15:29 +0800

    update DLinear scripts

---
+++ scripts/ETTm1/DLinear.sh
@@ -0,0 +1,49 @@
+export CUDA_VISIBLE_DEVICES=2
+
+python -u run.py \
+  --data_dir ./data/ETT-small/ \
+  --file_name ETTm1.csv \
+  --model DLinear \
+  --features M \
+  --seq_len 96 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --factor 3 \
+  --enc_in 7 \
+  --batch_size 64
+
+python -u run.py \
+  --data_dir ./data/ETT-small/ \
+  --file_name ETTm1.csv \
+  --model DLinear \
+  --features M \
+  --seq_len 96 \
+  --pred_len 192 \
+  --e_layers 2 \
+  --factor 3 \
+  --enc_in 7 \
+  --batch_size 64
+
+python -u run.py \
+  --data_dir ./data/ETT-small/ \
+  --file_name ETTm1.csv \
+  --model DLinear \
+  --features M \
+  --seq_len 96 \
+  --pred_len 336 \
+  --e_layers 2 \
+  --factor 3 \
+  --enc_in 7 \
+  --batch_size 64
+
+python -u run.py \
+  --data_dir ./data/ETT-small/ \
+  --file_name ETTm1.csv \
+  --model DLinear \
+  --features M \
+  --seq_len 96 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --factor 3 \
+  --enc_in 7 \
+  --batch_size 64
